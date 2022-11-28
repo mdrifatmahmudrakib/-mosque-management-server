eaf514e44b5bedaf98883aeb6cdd4ed9fdf18848
Rifat Mahmud <mdrifatmahmudrakib@gmail.com>
2022-11-29 04:16:52 +0600

    can update evets and campaign and JWT token added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -16,6 +18,23 @@ const uri = "mongodb+srv://mosque:7BSq1R4J2tdYLdOx@cluster0.8bklk.mongodb.net/?r
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 async function run() {
     try {
         await client.connect();
@@ -23,6 +42,23 @@ async function run() {
         const helpCollection = client.db('allCampaign').collection('campaign');
         const eventCollection = client.db('allCampaign').collection('events');
         const expertsCollection = client.db('allCampaign').collection('experts');
+        const userCollection = client.db('allCampaign').collection('users');
+
+        //user info save to database 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+
+            // res.send(result);
+        })
 
 
 
@@ -35,27 +71,40 @@ async function run() {
             res.send(campaignes);
         });
 
-        app.put('/campaign/:id', async (req, res) => {
-            const id = req.params.id;
-            const data = req.body;
+        // app.put('/campaign/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const data = req.body;
+        //     const filter = { _id: ObjectId(id) };
+        //     const updateDoc = {
+        //         $set: data
+        //     };
+        //     const options = { upsert: true };
+        //     const result = await helpCollection.updateOne(filter, updateDoc, options);
+        //     res.send(result);
+        // })
+
+
+        // app.get('/campaign/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const campaign = await helpCollection.findOne(query);
+        //     res.send(campaign);
+        // })
+
+        app.get('/allcampaign/:campaignId', async (req, res) => {
+            const id = req.params.campaignId;
             const filter = { _id: ObjectId(id) };
-            const updateDoc = {
-                $set: data
-            };
-            const options = { upsert: true };
-            const result = await helpCollection.updateOne(filter, updateDoc, options);
-            req.send(result);
+            const result = await helpCollection.findOne(filter);
+            res.send(result)
         })
 
 
-        app.get('/campaign/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const campaign = await helpCollection.findOne(query);
-            res.send(campaign);
-        })
-
-
+        // app.get('/events/:eventsId', async (req, res) => {
+        //     const id = req.params.eventsId;
+        //     const filter = { _id: ObjectId(id) };
+        //     const events = await eventCollection.findOne(filter);
+        //     res.send(events)
+        // })
 
 
 
@@ -68,6 +117,7 @@ async function run() {
         });
 
 
+
         //delete
         app.delete('/campaign/:id', async (req, res) => {
             const id = req.params.id;
@@ -78,6 +128,37 @@ async function run() {
         });
 
 
+        //update
+        app.put("/campaign/:id", async (req, res) => {
+            const id = req.params.id;
+            const campaign = req.body;
+
+            const result = await helpCollection.updateOne(
+                { _id: ObjectId(id) }, // Find Data by query many time query type is "_id: id" Cheack on database
+                {
+                    $set: campaign, // Set updated Data
+                },
+                { upsert: true } // define work
+            );
+            res.send({ result });
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // load all event from mongodb
 
         app.get('/events', async (req, res) => {
@@ -86,6 +167,55 @@ async function run() {
             const events = await cursor.toArray();
             res.send(events);
         });
+
+
+
+        app.get('/event/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const event = await eventCollection.findOne(query);
+            res.send(event);
+        })
+
+
+
+
+        //add event
+        app.post('/events', async (req, res) => {
+            const newevents = req.body;
+            const result = await eventCollection.insertOne(newevents);
+            res.send(result);
+        });
+
+
+
+        //delete event
+        app.delete('/event/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const event = await eventCollection.deleteOne(query);
+            res.send(event);
+        });
+
+
+
+        //update event
+        //update
+        app.put("/event/:id", async (req, res) => {
+            const id = req.params.id;
+            const event = req.body;
+
+            const result = await eventCollection.updateOne(
+                { _id: ObjectId(id) }, // Find Data by query many time query type is "_id: id" Cheack on database
+                {
+                    $set: event, // Set updated Data
+                },
+                { upsert: true } // define work
+            );
+            res.send({ result });
+        });
+
 
 
         // load all experts from mongodb
